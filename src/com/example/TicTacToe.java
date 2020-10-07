608a2ff836b751c2f5d91ffb934cc5d2ade7d16a
mehakjit <mehakjitsingh@gmail.com>
2020-10-07 10:43:24 +0530

    [Mehakjit] Added: Show Board Option

--- src/com/example/TicTacToe.java
+++ src/com/example/TicTacToe.java
@@ -3,20 +3,31 @@ package com.example;
 import java.util.Scanner;
 
 public class TicTacToe {
-	
+	public static char[] board = new char[10];
+			
 	public static void createBoard() {
-		char[] board = new char[10];
-		for (int i = 1; i < 10; i++)
+		for (int i = 1; i < 10 ; i++)
 			board[i] = ' ';
 	}
 	public static char chooseOption() {
 		Scanner sc = new Scanner(System.in);
 		int option = sc.nextInt();
-		if(option == 1) 
+		if(option == 1) {
+			System.out.println("Congratulations!! You have Chosen X \n" +
+								"Let's Begin");
 			return 'X';
-			else
-				return 'Y';
 		}
+		else {
+			System.out.println("Congratulations!! You have Chosen Y \n"
+								+ "Let's Begin");
+				return 'Y';
+			}
+		}
+		public static void showBoard() {
+			for (int i=1; i<10 ;i = i+3) {
+				System.out.println("[" + board[i] + "] [" + board[i+1] + "] [" + board[i+2] + "]" );
+			}
+			}
 				
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe");
@@ -24,5 +35,7 @@ public class TicTacToe {
 		System.out.println("To Choose X Press 1"+ "\n" +
 							"To Choose O Press 2");
 		char playerOption = chooseOption();
+		char computerOption = (playerOption == 'X') ? 'O' :'Y';
+		showBoard();
 		}
 }
