d6fe69e57d3b6b7a359dd13548bfd60d86c89bc5
mehakjit <mehakjitsingh@gmail.com>
2020-10-07 09:17:12 +0530

    [Mehakjit] Added: Intiatal Position

--- src/com/example/TicTacToe.java
+++ src/com/example/TicTacToe.java
@@ -1,5 +1,25 @@
 package com.example;
 
 public class TicTacToe {
-
+	
+	public class UC1{
+		char board[] = new char[10];
+		char position1 = board[1] ;
+		char position2 = board[2] ;
+		char position3 = board[3] ;
+		char position4 = board[4] ;
+		char position5 = board[5] ;
+		char position6 = board[6] ;
+		char position7 = board[7] ;
+		char position8 = board[8] ;
+		char position9 = board[9] ;
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to Tic Tac Toe");
+		
+		
+	}
 }
