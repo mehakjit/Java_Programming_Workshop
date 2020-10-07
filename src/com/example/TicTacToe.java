16ce73f4659bedd03c9cb5156cd52f18069267a5
mehakjit <mehakjitsingh@gmail.com>
2020-10-07 09:24:28 +0530

    [Mehakjit] Refactored: Intiatal Position

--- src/com/example/TicTacToe.java
+++ src/com/example/TicTacToe.java
@@ -2,24 +2,13 @@ package com.example;
 
 public class TicTacToe {
 	
-	public class UC1{
-		char board[] = new char[10];
-		char position1 = board[1] ;
-		char position2 = board[2] ;
-		char position3 = board[3] ;
-		char position4 = board[4] ;
-		char position5 = board[5] ;
-		char position6 = board[6] ;
-		char position7 = board[7] ;
-		char position8 = board[8] ;
-		char position9 = board[9] ;
-		
-		
+	public void UC1(){
+		char[] board = new char[10];
+		for(int i=1;i<10;i++)
+			board[i]=' ';		
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe");
-		
-		
-	}
+		}
 }
